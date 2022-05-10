feec61469977dadb30409d51803bc9a3c1c34ff3
Andrea Borruso <aborruso@gmail.com>
2022-05-10 15:15:20 +0200

    aggiunti localizzazioni open cup e cig anac

--- script/opencup/progetti-opencup.sh
+++ script/opencup/progetti-opencup.sh
@@ -30,5 +30,60 @@ qsvlite search -d ";" -i -s CODICE_LOCALE_PROGETTO PNRR "$folder"/../tmp/"$file"
 qsv sort -s CUP "$folder"/../../dati/"$name"/"$name"-PNRR.csv >"$folder"/../../dati/"$name"/tmp.csv
 mv "$folder"/../../dati/"$name"/tmp.csv "$folder"/../../dati/"$name"/"$name"-PNRR.csv
 
+# Aggiungi URL progetto OpenCUP
+mlr -I --csv put '$URL="https://opencup.gov.it/progetto/-/cup/".$CUP' then reorder -f URL "$folder"/../../dati/"$name"/"$name"-PNRR.csv
+
+# Cancella temporanei
+find "$folder"/../tmp -iname "*.*" -type f -delete
+
+### localizzazioni ###
+
+nameLoc="opencup-localizzazione"
+localizzazione="https://opencup.gov.it/documents/21195/117121/OpenCUP_Localizzazione.zip"
+
+# Scarica dati
+wget -O "$folder"/../tmp/"$nameLoc".zip "$localizzazione"
+# Decomprimi dati
+unzip "$folder"/../tmp/"$nameLoc".zip  -d "$folder"/../tmp
+
+# path file estratto
+file=$(find "$folder"/../tmp -iname "*.csv" -type f)
+
+qsvlite fmt -d ";" "$file" >"$folder"/../tmp/tmp.csv
+
+mv "$folder"/../tmp/tmp.csv "$file"
+
+qsvlite join CUP "$folder"/../../dati/"$name"/"$name"-PNRR.csv CUP "$file" >"$folder"/../tmp/tmp.csv
+
+header=$(<"$file" head -n 1)
+
+mlr --csv cut -f "$header" "$folder"/../tmp/tmp.csv >"$folder"/../../dati/"$name"/"$name"-PNRR-localizzazioni.csv
+
+# Cancella temporanei
+find "$folder"/../tmp -iname "*.*" -type f -delete
+
+### CIG ###
+
+nameCIG="anac-cig"
+URLcig="https://dati.anticorruzione.it/opendata/download/dataset/cup/filesystem/cup_csv_0.zip"
+
+# Scarica dati
+wget -O "$folder"/../tmp/"$nameCIG".zip "$URLcig"
+# Decomprimi dati
+unzip "$folder"/../tmp/"$nameCIG".zip  -d "$folder"/../tmp
+
+# path file estratto
+file=$(find "$folder"/../tmp -iname "*.csv" -type f)
+
+qsvlite fmt -d ";" "$file" >"$folder"/../tmp/tmp.csv
+
+mv "$folder"/../tmp/tmp.csv "$file"
+
+qsvlite join CUP "$folder"/../../dati/"$name"/"$name"-PNRR.csv CUP "$file" >"$folder"/../tmp/tmp.csv
+
+header=$(<"$file" head -n 1)
+
+mlr --csv cut -f "$header" "$folder"/../tmp/tmp.csv >"$folder"/../../dati/"$name"/"$name"-PNRR-CIG.csv
+
 # Cancella temporanei
 find "$folder"/../tmp -iname "*.*" -type f -delete
